e8fa7b78ec4358adbedd27fc77bae4f4cf22cb93
Malavikat6 <malavika.t@gadgeon.com>
2022-10-16 15:48:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demoapi';
+  col='yellow';
+  constructor(private http:HttpClient){
+
+  }
+   posts:any[]=[];
+  loadPosts()
+  {
+    this.http.get('http://localhost:8080/allbooks').subscribe((posts:any)=>
+    {
+      this.posts=posts;
+      // alert(JSON.stringify(res))
+    });
+  }
+}
